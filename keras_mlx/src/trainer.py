891dbfd3e112c4e1c6c168209dc138452e6696d7
Fabien Hertschuh <1091026+hertschuh@users.noreply.github.com>
2026-08-31 17:59:08 -0700

    Changes for multi-backend generalization.
    - Removed the layer super class, which is no longer needed.
    - Removed the `SavedModelExportArchive` sub-class, which is no longer needed.
    - Renames to the trainer classes.

--- keras_mlx/src/trainer.py
+++ keras_mlx/src/trainer.py
@@ -8,17 +8,17 @@ from keras.src import optimizers as optimizers_module
 from keras.src import tree
 from keras.src.backend import config
 from keras.src.backend.common.stateless_scope import StatelessScope
-from keras.src.trainers import trainer as base_trainer
 from keras.src.trainers.data_adapters import array_slicing
 from keras.src.trainers.data_adapters import data_adapter_utils
 from keras.src.trainers.epoch_iterator import EpochIterator
+from keras.src.trainers.trainer import BaseTrainer
 from keras.src.utils import traceback_utils
 from keras.src.utils.python_utils import pythonify_logs
 from keras_mlx.src.ops.core import convert_to_numpy
 from keras_mlx.src.ops.core import convert_to_tensor
 
 
-class MLXTrainer(base_trainer.Trainer):
+class Trainer(BaseTrainer):
     def __init__(self):
         super().__init__()
         self.train_function = None
